0eedd6da73a1e889328d19dc3dc1b6ebc9942e30
Akkay <nguyenhung.dev.info@gmail.com>
2024-02-02 19:42:02 +0700

    LinearLayout

---
+++ LinearLayout/app/src/main/java/com/example/linearlayout/NoWeightSum_Orientation_Vertical.java
@@ -0,0 +1,14 @@
+package com.example.linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoWeightSum_Orientation_Vertical extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_weight_sum_orientation_vertical);
+    }
+}
