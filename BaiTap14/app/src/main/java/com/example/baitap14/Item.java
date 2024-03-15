4144ebc25388d82dc5dd93461dfc46b493c7ff7d
Akkay <nguyenhung.dev.info@gmail.com>
2024-03-15 17:50:24 +0700

    BaiTap14

---
+++ BaiTap14/app/src/main/java/com/example/baitap14/Item.java
@@ -0,0 +1,15 @@
+package com.example.baitap14;
+
+public class Item {
+    private String content;
+
+    public Item(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
+
+
